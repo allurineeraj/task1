93190ab2d7731d212b9d544c9c14412985e02f1d
allurineeraj <nameisneeraj09@gmail.com>
2026-02-02 21:32:40 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+
+num1 = int(input("Enter first Number:"))
+num2 = int(input("Enter second Number:"))
+
+sum = num1+num2
+sub = num1-num2
+mul = num1*num2
+div = num1/num2
+print("Addition :"+ str(sum))
+print("Subtraction:"+str(sub))
+print("Multipication:"+str(mul))
+print("Division:"+str(div))
---
+++ task2.py
@@ -0,0 +1,5 @@
+firstname = input("Enter first name: ")
+lastname =input("Enter last name:")
+
+Name = firstname+" "+lastname
+print("Hello "+Name+ "! welcome to python")
